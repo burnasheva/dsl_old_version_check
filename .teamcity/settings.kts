97166b8f4bf30487e8e915730773f3a53d14ed3a
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2022-02-26 20:38:20 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -13,28 +13,6 @@ import jetbrains.buildServer.configs.kotlin.sharedResource
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2021.2"
 
 project {
@@ -48,6 +26,7 @@ project {
         buildReportTab {
             id = "PROJECT_EXT_1"
             title = "Code Coverage"
+            // комментарий на русском
             startPage = "coverage.zip!index.html"
         }
         dockerECRRegistry {
@@ -64,7 +43,7 @@ project {
         sharedResource {
             id = "PROJECT_EXT_4"
             name = "abc_abc_rename"
-            enabled = true
+            enabled = false
             resourceType = quoted(3)
         }
         youtrack {
